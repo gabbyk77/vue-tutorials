0d9fae331ffb1a6c70dd590fcceb810bf611d4fe
gabbyk77 <56268909+gabbyk77@users.noreply.github.com>
2022-03-11 12:51:41 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,17 +6,29 @@ const app = Vue.createApp({
       showBooks: true,
       title: 'Naruto',
       author: 'Masashi Kishimoto',
-      age: 45
+      age: 45,
+      x: 0,
+      y: 0
     }
   },
   methods: {
     toggleShowBooks() {
       this.showBooks = !this.showBooks
-    }
+    },
     // changeTitle(abc) {
     //   //this.title = 'Words of Radiance'
     //   this.title = abc
-    //}
+
+    handleEvent(e, data) {
+      console.log(e, e.type)
+      if (data) {
+        console.log(data)
+      }
+    },
+    handleMousemove(e) {
+      this.x = e.offsetX
+      this.y = e.offsetY
+    }
   }
 })
 
