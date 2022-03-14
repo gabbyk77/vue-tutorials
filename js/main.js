ecb61c12fe33f1bf97006a197221f0bdd4f1f4c7
gabbyk77 <56268909+gabbyk77@users.noreply.github.com>
2022-03-14 11:58:49 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,12 +3,18 @@ const app = Vue.createApp({
   data() {
     return {
       //creating an object with the curly brackets ^
+      url: 'http://www.thenetninja.co.uk',
       showBooks: true,
-      title: 'Naruto',
-      author: 'Masashi Kishimoto',
-      age: 45,
-      x: 0,
-      y: 0
+      books: [
+        {title: 'name of the wind', author: 'patrick rothfuss', img: 'img/xiao.jpg'},
+        {title: 'the way of kings', author: 'brandon sanderson', img: 'img/sonic.jpg'},
+        {title: 'the final empire', author: 'snjdavkdvnfd', img: 'img/gen.jpg'},
+      ]
+      // title: 'Naruto',
+      // author: 'Masashi Kishimoto',
+      // age: 45,
+      // x: 0,
+      // y: 0
     }
   },
   methods: {
